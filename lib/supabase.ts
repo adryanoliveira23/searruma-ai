efd4a308ff02d2dd57ee23effa7073237b83f514
Adryan Oliveira <blessedt402@gmail.com>
2026-02-05 15:19:34 -0300

    fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,10 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  "https://fkmjxfcbuppampdbvbuo.supabase.co";
+  "https://amufvgzrxeipylqmcfvx.supabase.co";
 const supabaseAnonKey =
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrbWp4ZmNidXBwYW1wZGJ2YnVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDkzNjcsImV4cCI6MjA4NTg4NTM2N30.OakbKmtfNXbcYqLgoQN08G24htsWqlPaI_WnygvoPuY";
+  "sb_publishable_cO-3U4wg8kqXvcxJrLHayg_7WcLJIS6";
 
 // Client for client-side/public usage
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- middleware.ts
+++ middleware.ts
@@ -9,8 +9,10 @@ export async function middleware(request: NextRequest) {
   });
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL ||
+      "https://amufvgzrxeipylqmcfvx.supabase.co",
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+      "sb_publishable_cO-3U4wg8kqXvcxJrLHayg_7WcLJIS6",
     {
       cookies: {
         get(name: string) {
