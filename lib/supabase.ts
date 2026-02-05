e76093e28fab3a998c195388b0616a109de77829
Adryan Oliveira <blessedt402@gmail.com>
2026-02-05 15:11:13 -0300

    fix

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://fkmjxfcbuppampdbvbuo.supabase.co";
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrbWp4ZmNidXBwYW1wZGJ2YnVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAzMDkzNjcsImV4cCI6MjA4NTg4NTM2N30.OakbKmtfNXbcYqLgoQN08G24htsWqlPaI_WnygvoPuY";
 
 // Client for client-side/public usage
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
